ac58af9129d909338c4728a71be1272681de3bf4
doragrishaeva <darya-grishaeva@mail.ru>
2020-12-09 12:39:57 +0300

    fix link

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = new Vue({
     el: '#portfolio',
     data: {
         projects: [
-            {id : 1, name: 'Mortgage Calculator', img: 'images/mortgage.png', github:'https://github.com/doragrishaeva/mortgage-calculator', link: 'https://doragrishaeva.github.io/mortgage-calculator/', description: "Example of mortgage calculator on Vue js", categories: ['Frameworks'], subCategories: ['BEM Methodology', 'Adaptive design', 'SCSS', 'Vue JS'] },
+            {id : 1, name: 'Mortgage Calculator', img: 'images/mortgage.PNG', github:'https://github.com/doragrishaeva/mortgage-calculator', link: 'https://doragrishaeva.github.io/mortgage-calculator/', description: "Example of mortgage calculator on Vue js", categories: ['Frameworks'], subCategories: ['BEM Methodology', 'Adaptive design', 'SCSS', 'Vue JS'] },
             {id : 2, name: 'Shopping List', img: 'images/jquery-table.JPG', github:'https://github.com/doragrishaeva/jquery-table', link: 'https://doragrishaeva.github.io/jquery-table/', description: 'A single page app for shopping built on JQuery', categories: ['Components'], subCategories: ['JQuery', 'Bootstrap'] },
             {id: 3, name: 'Ravenous', img: 'images/ravenous.PNG', github:'https://github.com/doragrishaeva/ravenous', link: 'https://doragrishaeva.github.io/ravenous/', description: 'Restaurant searcher on React.js', categories: ['Frameworks', 'REST API'], subCategories: ['React JS', 'REST API']},
             {id : 4, name: 'Singolo', img: 'images/singolo.png', github:'https://github.com/doragrishaeva/singolo', link: 'https://doragrishaeva.github.io/singolo/', description: "Example of single page markup - Singolo", categories: ['Markup'], subCategories: ['BEM Methodology', 'JQuery', 'Adaptive design'] },
